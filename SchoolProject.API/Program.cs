ad43d7ea061511cd7e62b6b16dde66293dadb704
Ahmed Elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-07-28 22:20:56 +0300

    updated database

--- SchoolProject.API/Program.cs
+++ SchoolProject.API/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using SchoolProject.Infrastructure.Data;
+
 namespace SchoolProject.API
 {
     public class Program
@@ -13,7 +16,11 @@ namespace SchoolProject.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            //Sql Connection 
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("default"));
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
---
+++ SchoolProject.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolProject.Data.Entities;
+
+namespace SchoolProject.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subjects> Subjects { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<DepartmetSubject> DepartmentSubjects { get; set; }
+        public DbSet<StudentSubject> StudentSubjects { get; set; }
+    }
+}
